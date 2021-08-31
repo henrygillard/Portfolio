c71ef2565f339f5e1a794076e53f338b73ea7317
Henry Gillard <henry@MacBook-Pro.local>
2021-08-31 15:23:46 -0700

    try again

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,10 +2,7 @@ import { Link } from "react-router-dom";
 
 // Not destructuring props this time
 export default function NavBar(props) {
-  function handleLogOut() {
-    userService.logOut();
-    props.setUser(null);
-  }
+ 
 
   return (
     <nav>
