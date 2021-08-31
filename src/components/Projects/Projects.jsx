2886fdfa91c7b519bc9fc4974ac0506f104dedf3
Henry Gillard <henry@MacBook-Pro.local>
2021-08-31 12:15:08 -0700

    project links

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,5 +1,12 @@
 export default function Projects() {
     return(
-        <div>My Projects</div>
+        <div>
+            <h1>My Projects</h1>
+            <div className="links"></div>
+            <div className="project-card"><a href="http://alephnullvacations.herokuapp.com/">Aleph Null Vacations</a></div>
+            <div className="project-card"><a href="https://henrygillard.github.io/BlackJackGame/">Casino BlackJack</a></div>
+            <div className="project-card"><a href="https://pretty-palette-picker.herokuapp.com/">Palette Picker</a></div>
+            <div className="project-card"><a href="https://henrys-recipe-shopping-list.herokuapp.com/">Shop-List</a></div>
+            </div>
     )
 }
