65a2fe96ecfb3b20f56d905bcdd9c231f8127287
Henry Gillard <henry@MacBook-Pro.local>
2021-08-31 15:41:31 -0700

    try again

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import GitHubIcon from '@material-ui/icons/GitHub'
 
 // Not destructuring props this time
 export default function NavBar(props) {
@@ -9,6 +10,7 @@ export default function NavBar(props) {
       <Link to="/projects">My Projects</Link>
       &nbsp; | &nbsp;
       <Link to="/about">About Me</Link>
+      <a href="https://github.com/henrygillard"><GitHubIcon /></a>
     </nav>
   );
 }
