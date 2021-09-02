6f042fa9869b348777e0101c392858aa353ca352
Henry Gillard <henry@MacBook-Pro.local>
2021-09-02 10:59:01 -0700

    More Style

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,6 +9,7 @@ export default function Projects() {
             <div className="images">
                 <img src="https://i.imgur.com/t19hd23.jpg" alt="me-1" style={{width: "25rem", height: '32rem'}}></img>
             </div>
+           
             <p>I am an aspiring Software Developer, 
                 recently graduated form General Assembly’s Software Engineering Immersive Program, 
                 and a Musician. I love blending my passion for creativity/art with tech. 
@@ -18,7 +19,7 @@ export default function Projects() {
                 I enjoy the creative process!</p>
             <div className="about-me-links">
                 
-                <a href="https://github.com/henrygillard" target="_blank" rel="noreferrer"><GitHubIcon /></a>
+                <a href="https://github.com/henrygillard" target="_blank" rel="noreferrer" style={{borderRight: 'solid thin'}}><GitHubIcon /></a>
                
                 <a href="https://www.linkedin.com/in/henrygillard/" target="_blank" rel="noreferrer"><LinkedInIcon /></a>
             </div>
