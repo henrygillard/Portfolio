68d16ffdbd4e8535e5ef8b15be5d3da9a1192079
Henry Gillard <henry@MacBook-Pro.local>
2021-09-02 10:26:40 -0700

    More Style

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,8 +1,10 @@
 import "./About.css"
+import GitHubIcon from '@material-ui/icons/GitHub'
+import LinkedInIcon from "@material-ui/icons/LinkedIn"
 
 export default function Projects() {
     return(
-        <div>
+        <div className="container">
             <h1>About Me</h1>
             <div className="images">
                 <img src="https://i.imgur.com/t19hd23.jpg" alt="me-1" style={{width: "25rem", height: '32rem'}}></img>
@@ -14,6 +16,12 @@ export default function Projects() {
                 and are executed with simplicity. I’m competitive with mostly everything I try to create, 
                 but it is not my main motivating factor to create great work. Put simply, 
                 I enjoy the creative process!</p>
+            <div className="about-me-links">
+                
+                <a href="https://github.com/henrygillard" target="_blank" rel="noreferrer"><GitHubIcon /></a>
+               
+                <a href="https://www.linkedin.com/in/henrygillard/" target="_blank" rel="noreferrer"><LinkedInIcon /></a>
+            </div>
         </div>
     )
 }
