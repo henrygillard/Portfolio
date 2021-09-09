042fe14bb1e106677a327a8191db0e9549b1d15a
Henry Gillard <henry@MacBook-Pro.local>
2021-09-09 09:15:54 -0700

    checkout fix

--- server.js
+++ server.js
@@ -41,12 +41,7 @@ app.use(express.static('public'));
 const YOUR_DOMAIN = 'https://henrygillard.herokuapp.com/checkout';
 app.post('/create-checkout-session', async (req, res) => {
   const session = await stripe.checkout.sessions.create({
-    customer_email: 'customer@example.com',
     submit_type: 'donate',
-    billing_address_collection: 'auto',
-    shipping_address_collection: {
-      allowed_countries: ['US', 'CA'],
-    },
     line_items: [
       {
         price: 'price_1JXDc9HofPNnQGYeXifca6Vi',
